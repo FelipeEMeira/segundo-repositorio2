abfe61513bd34a1f2aecd68ec146282799d02228
Felipe <felipe.eduardo.meira@escola.pr.gov.br>
2025-06-11 21:12:49 -0300

    11/06 perguntas

---
+++ perguntas.js
@@ -0,0 +1,89 @@
+criaCartao(
+'HTML', 
+'O que significa HTML?', 
+'HTML significa HyperText Markup Language.'
+);
+
+criaCartao(
+'HTML', 
+'Para que serve a tag <a>?', 
+'A tag <a> é usada para criar hiperlinks, permitindo a navegação entre páginas.'
+);
+
+criaCartao(
+'HTML', 
+'O que faz a tag <img>?', 
+'Insere uma imagem na página web.'
+);
+
+criaCartao(
+'HTML', 
+'Qual é o propósito da tag <head>?', 
+'Contém metadados sobre o documento, como título, links e scripts.'
+);
+
+criaCartao(
+'HTML', 
+'Como se define um campo de entrada de texto em um formulário?', 
+'Usa-se a tag <input type="text">.'
+);
+
+criaCartao(
+'CSS', 
+'O que significa CSS?', 
+'CSS significa Cascading Style Sheets.'
+);
+
+criaCartao(
+'CSS', 
+'Como se aplica uma cor de fundo a um elemento?', 
+'Com a propriedade CSS background-color.'
+);
+
+criaCartao(
+'CSS', 
+'O que faz a propriedade display: flex?', 
+'Torna o contêiner um flex container, permitindo layout flexível dos filhos.'
+);
+
+criaCartao(
+'CSS', 
+'Qual a diferença entre padding e margin?', 
+'Padding é o espaço interno do elemento; margin é o espaço externo.'
+);
+
+criaCartao(
+'CSS', 
+'Como se seleciona todos os parágrafos de uma página em CSS?', 
+'Usa-se o seletor: p { ... }'
+);
+
+criaCartao(
+'JavaScript', 
+'O que é JavaScript?', 
+'É uma linguagem de programação usada para tornar páginas web interativas.'
+);
+
+criaCartao(
+'JavaScript', 
+'Como declarar uma variável em JavaScript?', 
+'Pode-se usar var, let ou const.'
+);
+
+criaCartao(
+'JavaScript', 
+'O que faz a função console.log()?', 
+'Exibe uma mensagem no console do navegador.'
+);
+
+criaCartao(
+'JavaScript', 
+'Como se define uma função em JavaScript?', 
+'function nomeFuncao() { /* código */ }'
+);
+
+criaCartao(
+'JavaScript', 
+'O que é o DOM em JavaScript?', 
+'É a interface que representa a estrutura do documento HTML para ser manipulada com JS.'
+);
